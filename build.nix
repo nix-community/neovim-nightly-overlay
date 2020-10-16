dc66d7410dd109ca682aa438d0b8a0c02c35694f
Michael Lingelbach <m.j.lbach@gmail.com>
2020-10-16 11:47:48 -0700

    Update/fix CI

--- build.nix
+++ build.nix
@@ -2,7 +2,7 @@ let
   sources = import ./nix/sources.nix;
   nixpkgs = sources."nixpkgs-unstable";
   neovim-nightly-overlay = import ./default.nix;
-  pkgs = import nixpkgs { config = {}; overlays = [ neovim-nightly-overlay ]; };
+  pkgs = import nixpkgs { config = { }; overlays = [ neovim-nightly-overlay ]; };
 in
 {
   neovim-nightly = pkgs.neovim-nightly;
--- default.nix
+++ default.nix
@@ -1,17 +1,17 @@
 let
   sources = import ./nix/sources.nix;
   nixpkgs = sources."nixpkgs-unstable";
-  pkgs = import nixpkgs {};
+  pkgs = import nixpkgs { };
 in
 _: _:
-  {
-    neovim-nightly = pkgs.neovim-unwrapped.overrideAttrs (
-      _: {
-        pname = "neovim-nightly";
-        version = "master";
-        src = pkgs.fetchFromGitHub {
-          inherit (sources.neovim) owner repo rev sha256;
-        };
-      }
-    );
-  }
+{
+  neovim-nightly = pkgs.neovim-unwrapped.overrideAttrs (
+    _: {
+      pname = "neovim-nightly";
+      version = "master";
+      src = pkgs.fetchFromGitHub {
+        inherit (sources.neovim) owner repo rev sha256;
+      };
+    }
+  );
+}
