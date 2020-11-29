81f21dbe06f10adb5ebe7fdb269c2489e0778bef
Michael Lingelbach <m.j.lbach@gmail.com>
2020-11-28 17:52:29 -0800

    Migrate to flakes

--- build.nix
+++
@@ -1,9 +0,0 @@
-let
-  sources = import ./nix/sources.nix;
-  nixpkgs = sources."nixpkgs-unstable";
-  neovim-nightly-overlay = import ./default.nix;
-  pkgs = import nixpkgs { config = { }; overlays = [ neovim-nightly-overlay ]; };
-in
-{
-  neovim-nightly = pkgs.neovim-nightly;
-}
