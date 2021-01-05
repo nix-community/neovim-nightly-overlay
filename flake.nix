ee72794a7e7ba90295645fd5d87fb1b5db6af267
Michael Lingelbach <m.j.lbach@gmail.com>
2021-01-04 17:44:47 -0800

    flakes: align with neovim core, add additional flake outputs

--- flake.nix
+++ flake.nix
@@ -1,30 +1,81 @@
 {
-  description = "Neovim nightly overlay";
+  description = "Neovim flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.neovim-nightly = { url = "github:neovim/neovim"; flake = false; };
-  inputs.gitignore = { url = "github:hercules-ci/gitignore.nix"; flake = false;};
-  inputs.pre-commit-hooks= { url = "github:cachix/pre-commit-hooks.nix"; flake = false;};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, ... }@inputs:
-    with inputs;
+  outputs = { self, nixpkgs, flake-utils }:
     {
       overlay = final: prev:
         let
-          pkgs = inputs.nixpkgs.legacyPackages.${prev.system};
+          pkgs = nixpkgs.legacyPackages.${prev.system};
         in
-        {
-          neovim-nightly = pkgs.neovim-unwrapped.overrideAttrs (
-            old: {
-              pname = "neovim-nightly";
-              version = "master";
-              src = inputs.neovim-nightly;
+        rec {
+          neovim = pkgs.neovim-unwrapped.overrideAttrs (oa: {
+            version = "master";
+            src = ../.;
 
-              buildInputs = old.buildInputs ++ [ pkgs.tree-sitter ];
-            }
-          );
+            buildInputs = oa.buildInputs ++ ([
+              pkgs.tree-sitter
+            ]);
+
+            cmakeFlags = oa.cmakeFlags ++ [
+              "-DUSE_BUNDLED=OFF"
+            ];
+          });
+
+          # a development binary to help debug issues
+          neovim-debug = let
+            stdenv = pkgs.stdenvAdapters.keepDebugInfo (if pkgs.stdenv.isLinux then pkgs.llvmPackages_latest.stdenv else pkgs.stdenv);
+          in
+            pkgs.enableDebugging ((neovim.override {
+            lua = pkgs.enableDebugging pkgs.luajit;
+            inherit stdenv;
+          }).overrideAttrs (oa: {
+            cmakeBuildType = "Debug";
+            cmakeFlags = oa.cmakeFlags ++ [
+              "-DMIN_LOG_LEVEL=0"
+            ];
+
+            disallowedReferences = [];
+          }));
+
+          # for neovim developers, very slow
+          # brings development tools as well
+          neovim-developer =
+            let
+              lib = nixpkgs.lib;
+              pythonEnv = pkgs.python3;
+              luacheck = pkgs.luaPackages.luacheck;
+            in
+            (neovim-debug.override ({ doCheck = pkgs.stdenv.isLinux; })).overrideAttrs (oa: {
+              cmakeFlags = oa.cmakeFlags ++ [
+                "-DLUACHECK_PRG=${luacheck}/bin/luacheck"
+                "-DMIN_LOG_LEVEL=0"
+                "-DENABLE_LTO=OFF"
+                "-DUSE_BUNDLED=OFF"
+              ] ++ pkgs.stdenv.lib.optionals pkgs.stdenv.isLinux [
+                # https://github.com/google/sanitizers/wiki/AddressSanitizerFlags
+                # https://clang.llvm.org/docs/AddressSanitizer.html#symbolizing-the-reports
+                "-DCLANG_ASAN_UBSAN=ON"
+              ];
+
+              nativeBuildInputs = oa.nativeBuildInputs ++ (with pkgs; [
+                pythonEnv
+                include-what-you-use # for scripts/check-includes.py
+                jq # jq for scripts/vim-patch.sh -r
+                doxygen
+              ]);
+
+              shellHook = oa.shellHook + ''
+                export NVIM_PYTHON_LOG_LEVEL=DEBUG
+                export NVIM_LOG_FILE=/tmp/nvim.log
+                export ASAN_OPTIONS="log_path=./test.log:abort_on_error=1"
+                export UBSAN_OPTIONS=print_stacktrace=1
+              '';
+            });
         };
     } //
     flake-utils.lib.eachDefaultSystem (system:
@@ -34,9 +85,22 @@
           inherit system;
         };
       in
-      {
-        defaultPackage = pkgs.neovim-nightly;
-        devShell = import ./shell.nix { inherit pkgs inputs; };
+      rec {
+
+        packages = with pkgs; {
+          inherit neovim neovim-debug neovim-developer;
+        };
+
+        defaultPackage = pkgs.neovim;
+
+        apps = {
+          nvim = flake-utils.lib.mkApp { drv = pkgs.neovim; name = "nvim"; };
+          nvim-debug = flake-utils.lib.mkApp { drv = pkgs.neovim-debug; name = "nvim"; };
+        };
+
+        defaultApp = apps.nvim;
+
+        devShell = pkgs.neovim-developer;
       }
     );
 }
