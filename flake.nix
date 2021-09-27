202fdf9aa77165fe6b663f016a52644c7f7af101
Matthieu Coudron <mcoudron@hotmail.com>
2021-09-27 19:26:57 +0200

    added a vimPlugins overlay that targets neovim master
    we could retreive instead the `vim-plugin-names` from nixpkgs and strips
    it of its 0.5-compat branch.

--- flake.nix
+++ flake.nix
@@ -11,11 +11,20 @@
   outputs = { self, nixpkgs, neovim-flake, ... }:
     rec {
       inherit (neovim-flake) packages defaultPackage apps defaultApp devShell;
-      overlay = final: prev: rec {
+      overlay = final: prev: let
+        pkgs = "${prev.system}".pkgs;
+      in {
         neovim-unwrapped = neovim-flake.packages.${prev.system}.neovim;
         neovim-nightly = neovim-flake.packages.${prev.system}.neovim;
         neovim-debug = neovim-flake.packages.${prev.system}.neovim-debug;
         neovim-developer = neovim-flake.packages.${prev.system}.neovim-developer;
+
+        # nixpkgs pins plugins to work with their matching neovim version
+        # this tries to get the latest version of the plugins so that they work
+        # with neovim master
+        vimPluginsForNeovimMaster = pkgs.callPackage ./vim-plugins/generated.nix {
+          inherit (pkgs.vimUtils) buildVimPluginFrom2Nix;
+        };
       };
     };
 }
