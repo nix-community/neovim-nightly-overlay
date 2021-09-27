31a96ef6b272abac3bd6516e75d2d6bbaa54aa9c
L <91376574+LeoTzahal18@users.noreply.github.com>
2021-09-27 16:09:00 -0300

    Create Rota.vue

---
+++ Rota.vue
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const rota = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // rota level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this rota
+    // which is lazy-loaded when the rota is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+  }
+]
+
+const rota = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+
+export to Nago
+export to Mu
+Export tô FBI
+export default rota
+export to Union
+export to (Android MacOSGold) & make a &new file(GPS.mundi) in androidmacosgold
