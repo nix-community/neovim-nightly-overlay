12ad6c5b1f00909300e022a38cf0ae5231033faa
Ilan Joselevich <personal@ilanjoselevich.com>
2023-04-13 15:23:30 +0300

    Revert "flake.nix: add build fix to overlays too"
    This reverts commit 0c7f9fe76d10b50a17711bae55cbd36e33e10e2c.

--- flake.nix
+++ flake.nix
@@ -14,8 +14,8 @@
       packages = forAllSystems (system: { inherit (neovim-flake.packages.${system}) default neovim; });
       defaultPackage = forAllSystems (system: neovim-flake.packages.${system}.default);
       overlay = final: prev: {
-        neovim-unwrapped = self.packages.${prev.system}.neovim;
-        neovim-nightly = self.packages.${prev.system}.neovim;
+        neovim-unwrapped = neovim-flake.packages.${prev.system}.neovim;
+        neovim-nightly = neovim-flake.packages.${prev.system}.neovim;
       };
       herculesCI = {
         ciSystems = [ "x86_64-linux" "aarch64-linux" ]; # These are the only systems avaiable currently for our Hercules-CI infra.
