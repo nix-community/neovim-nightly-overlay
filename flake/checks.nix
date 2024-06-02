030a549acc77e5a7e88ed88c268d490812243e57
Gaetan Lepage <gaetan@glepage.com>
2024-06-03 00:12:35 +0200

    checks: remove (already disabled) shlint check

--- flake/checks.nix
+++ flake/checks.nix
@@ -1,16 +1,10 @@
-{inputs, ...}: {
+{
   perSystem = {
     pkgs,
     config,
     ...
   }: {
     checks = {
-      # TODO: not working: remove ?
-      # shlint = pkgs.runCommand "shlint" {
-      #   nativeBuildInputs = [pkgs.shellcheck];
-      #   preferLocalBuild = true;
-      # } "make -C ${inputs.neovim-src} shlint > $out";
-
       # Not checking neovim-developer here as it currently failes because of memory leaks
       inherit
         (config.packages)
