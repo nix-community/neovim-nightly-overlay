c641f9c0d7127f146feb0a8b659a9d9950551fce
Gaetan Lepage <gaetan@glepage.com>
2024-12-12 15:04:13 +0100

    flake/default: nixfmt-rfc-style is the default

--- flake/default.nix
+++ flake/default.nix
@@ -23,10 +23,7 @@
         flakeCheck = true;
 
         programs = {
-          nixfmt = {
-            enable = true;
-            package = pkgs.nixfmt-rfc-style;
-          };
+          nixfmt.enable = true;
         };
       };
     }
