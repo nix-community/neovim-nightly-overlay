aa2dff26314697575c0c1205c32c17c4870ce1d9
Gaetan Lepage <gaetan@glepage.com>
2025-03-21 15:14:27 +0100

    TEST

--- flake.nix
+++ flake.nix
@@ -45,9 +45,9 @@
       {
         systems = [
           "x86_64-linux"
-          "x86_64-darwin"
-          "aarch64-linux"
-          "aarch64-darwin"
+          # "x86_64-darwin"
+          # "aarch64-linux"
+          # "aarch64-darwin"
         ];
 
         imports = [
--- flake/packages/neovim-debug.nix
+++ flake/packages/neovim-debug.nix
@@ -8,8 +8,15 @@
   lua = pkgs.luajit;
 }).overrideAttrs
   (oa: {
-    dontStrip = true;
+    # Build neovim in debug mode
     NIX_CFLAGES_COMPILE = " -ggdb -Og";
     cmakeBuildType = "Debug";
-    disallowedReferences = [ ];
+
+    # Prevent nix from stripping debug symbols from the final binary
+    dontStrip = false;
+
+    # Do not explicitly disallow any paths to be referenced by the output
+    # By default, the neovim derivation disallows any reference to the compiler.
+    # https://github.com/NixOS/nixpkgs/blob/519502452f88c1e0b29ea6021fac0ce1640f4881/pkgs/by-name/ne/neovim-unwrapped/package.nix#L193
+    disallowedRequisites = [ ];
   })
