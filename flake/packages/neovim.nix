d83810bcf7a46837e2c3962b5fd396a0677c219a
Gaetan Lepage <gaetan@glepage.com>
2025-09-11 14:17:59 +0200

    packages: expose the overriden tree-sitter package

--- flake/packages/neovim.nix
+++ flake/packages/neovim.nix
@@ -3,6 +3,7 @@
   lib,
   pkgs,
   neovim-dependencies,
+  tree-sitter,
   ...
 }:
 let
@@ -26,31 +27,7 @@ let
       # src = deps.libuv;
     };
     lua = pkgs.luajit;
-    tree-sitter =
-      (pkgs.tree-sitter.override {
-        rustPlatform = pkgs.rustPlatform // {
-          buildRustPackage =
-            args:
-            pkgs.rustPlatform.buildRustPackage (
-              args
-              // {
-                version = "bundled";
-                src = deps.treesitter;
-                cargoHash = "sha256-0Do1UxIbfIfJ61dTiJt0ZGDrhOtGV0l9bafyoqcbqgU=";
-              }
-            );
-        };
-      }).overrideAttrs
-        (oa: {
-          # Disable patches applied by the nixpkgs tree-sitter derivation as they clash with this revision of TS.
-          # The equivalent patching is done below in `postPatch`
-          patches = [ ];
-          postPatch = ''
-            ${oa.postPatch}
-            sed -e 's/playground::serve(.*$/println!("ERROR: web-ui is not available in this nixpkgs build; enable the webUISupport"); std::process::exit(1);/' \
-                -i cli/src/main.rs
-          '';
-        });
+    inherit tree-sitter;
 
     treesitter-parsers =
       let
