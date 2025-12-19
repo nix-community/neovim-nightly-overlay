8076e2ec697057ae18ae42224f5cc053521ea2c3
Bot_wxt1221 <3264117476@qq.com>
2025-12-20 06:50:54 +0800

    tree-sitter: fix eval problem (#1115)
    It has been failed for so long. 
    
    
    https://hercules-ci.com/github/nix-community/neovim-nightly-overlay/jobs/5519

--- flake/packages/tree-sitter.nix
+++ flake/packages/tree-sitter.nix
@@ -3,35 +3,32 @@
   pkgs,
   neovim-dependencies,
 }:
-(pkgs.tree-sitter.override {
-  rustPlatform = pkgs.rustPlatform // {
-    buildRustPackage =
-      args:
-      pkgs.rustPlatform.buildRustPackage (
-        args
-        // {
-          version = "bundled";
-          src = neovim-dependencies.treesitter;
-          cargoHash = "sha256-kHYLaiCHyKG+DL+T2s8yumNHFfndrB5aWs7ept0X4CM=";
-        }
-      );
+let
+  cargoHash = "sha256-kHYLaiCHyKG+DL+T2s8yumNHFfndrB5aWs7ept0X4CM=";
+in
+pkgs.tree-sitter.overrideAttrs (oa: {
+  src = neovim-dependencies.treesitter;
+  version = "bundled";
+  inherit cargoHash;
+  cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+    name = "${oa.pname}-cargo-deps";
+    src = neovim-dependencies.treesitter;
+    hash = cargoHash;
   };
-}).overrideAttrs
-  (oa: {
-    # Disable patches applied by the nixpkgs tree-sitter derivation as they clash with this revision of TS.
-    # The equivalent patching is done below in `postPatch`
-    patches = [ ];
+  # Disable patches applied by the nixpkgs tree-sitter derivation as they clash with this revision of TS.
+  # The equivalent patching is done below in `postPatch`
+  patches = [ ];
 
-    # clang is needed by the quickjs-sys crate to compile quickjs
-    nativeBuildInputs = [
-      pkgs.clang
-    ]
-    ++ oa.nativeBuildInputs;
-    env.LIBCLANG_PATH = "${lib.getLib pkgs.libclang}/lib";
+  # clang is needed by the quickjs-sys crate to compile quickjs
+  nativeBuildInputs = [
+    pkgs.clang
+  ]
+  ++ oa.nativeBuildInputs;
+  env.LIBCLANG_PATH = "${lib.getLib pkgs.libclang}/lib";
 
-    postPatch = ''
-      ${oa.postPatch}
-      sed -e 's/playground::serve(.*$/println!("ERROR: web-ui is not available in this nixpkgs build; enable the webUISupport"); std::process::exit(1);/' \
-          -i crates/cli/src/main.rs
-    '';
-  })
+  postPatch = ''
+    ${oa.postPatch}
+    sed -e 's/playground::serve(.*$/println!("ERROR: web-ui is not available in this nixpkgs build; enable the webUISupport"); std::process::exit(1);/' \
+        -i crates/cli/src/main.rs
+  '';
+})
