da39105fecb3d985ad4f71d31cab14863d968020
teto <886074+teto@users.noreply.github.com>
2026-04-06 22:27:32 +0200

    fix(devShell): the current
    current mkShell approach doesn't inherit cmakeFlags or cmake shell functions.
    Because I would like to expose possibly different dev configurations,
    I've added a function that takes as input a neovim derivation and
    modifies it to outputs a devShell.
    
    It removes for instance the patch to hardcode "version" in cmake.config/versiondef.h.in,  to minimize the noisy git diff when patching neovim

--- flake/packages/neovim-developer.nix
+++ flake/packages/neovim-developer.nix
@@ -23,6 +23,13 @@ neovim-debug.overrideAttrs (oa: {
 
   doCheck = pkgs.stdenv.isLinux;
 
+  shellHook = ''
+    ${oa.shellHook or ""}
+    export ASAN_SYMBOLIZER_PATH=${pkgs.llvm_18}/bin/llvm-symbolizer
+
+    # ASAN_OPTIONS=detect_leaks=1
+    export ASAN_OPTIONS="log_path=./test.log:abort_on_error=1"
+  '';
   # This package can be "failing" as soon as a memory leak is detected
   ignoreFailure = true;
 })
