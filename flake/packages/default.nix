d3367f69ec16ba005704e54e66e6983bd6a5abe6
teto <886074+teto@users.noreply.github.com>
2026-04-07 16:38:27 +0200

    fix neovim-* override interface
    we want users to be able to override neovim* packages (eg neovim-debug, neovim-developer)  like he/she overrides neovim-unwrapped
    
    For instance I override home-manager's neovim package with:
    
    
    ```
      package = flakeSelf.packages.${pkgs.stdenv.hostPlatform.system}.neovim.override
          {
            # we want to take the luajit with our overlay of lua packages
            luajit = pkgs.luajit.override {
              packageOverrides = import ./lua-overrides.nix;
            };
    
          }
    ```
    
    
    We use "import" instead of "callPackage" for neovim variants to keep the original "override" interface, ie, 
    This may change after https://github.com/NixOS/nixpkgs/pull/455805

--- flake/packages/default.nix
+++ flake/packages/default.nix
@@ -24,19 +24,24 @@
             inherit lib pkgs neovim-dependencies;
           };
 
+          # NOTE: We use "import" instead of "callPackage" for neovim variants to keep the original "override" interface, ie, a user shall should be able to override neovim-debug like he overrides neovim-unwrapped
+          # This may change after https://github.com/NixOS/nixpkgs/pull/455805
           neovim = import ./neovim.nix {
             inherit (inputs) neovim-src;
             inherit lib pkgs neovim-dependencies;
             inherit (config.packages) tree-sitter;
           };
 
-          neovim-debug = pkgs.callPackage ./neovim-debug.nix {
+          neovim-debug = import ./neovim-debug.nix {
             inherit (config.packages) neovim;
+            inherit (pkgs) stdenv llvmPackages_latest;
+            inherit lib;
           };
 
-          neovim-developer = pkgs.callPackage ./neovim-developer.nix {
+          neovim-developer = import ./neovim-developer.nix {
             inherit (config.packages) neovim-debug;
             inherit (inputs) neovim-src;
+            inherit lib pkgs;
           };
         };
     };
